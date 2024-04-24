ec4b68f16da230ddd72ca75455e2c86388f63c5c
victor <oguntayovictor65@gmail.com>
2024-04-24 02:08:42 -0700

    done with marketplace component

--- src/Data.js
+++ src/Data.js
@@ -3,6 +3,8 @@ import feature1 from './assets/feature1.jpg'
 import feature2 from "./assets/feature2.jpg";
 
 import feature3 from "./assets/feature3.jpg";
+import marketPlace1 from './assets/marketplace1.png'
+import marketPlace2 from "./assets/marketplace2.png";
 
 
 
@@ -21,5 +23,40 @@ export const nftFeatured = [
     image: feature3,
     id: 3,
     eth: 0.95,
+  },
+];
+
+export const marketPlace = [
+  {
+    id: 1,
+    image: marketPlace1,
+    title: "hot wheels",
+    text: "Hot Wheels NFTs are unique digital collectibles.Over the years. With the NFT Garage, they've made an epic leap from physical to digital collectibles, for a new generation of fans & collectors",
+    volume: "17MFLOW",
+    flow: "0.03fLOW",
+  },
+  {
+    id: 2,
+    image: marketPlace2,
+    title: "wanderers",
+    text: "Hot Wheels NFTs are unique digital collectibles.Over the years. With the NFT Garage, they've made an epic leap from physical to digital collectibles, for a new generation of fans & collectors",
+    volume: "4.9KETH",
+    flow: "O.O58ETH",
+  },
+  {
+    id: 3,
+    image: marketPlace1,
+    title: "wanderers",
+    text: "Hot Wheels NFTs are unique digital collectibles.Over the years. With the NFT Garage, they've made an epic leap from physical to digital collectibles, for a new generation of fans & collectors",
+    volume: "4.9KETH",
+    flow: "O.O58ETH",
+  },
+  {
+    id: 4,
+    image: marketPlace2,
+    title: "wanderers",
+    text: "Hot Wheels NFTs are unique digital collectibles.Over the years. With the NFT Garage, they've made an epic leap from physical to digital collectibles, for a new generation of fans & collectors",
+    volume: "4.9KETH",
+    flow: "O.O58ETH",
   },
 ];
--- src/component/MarketPlace.jsx
+++ src/component/MarketPlace.jsx
@@ -1,21 +1,85 @@
+import { marketPlace } from "../Data";
 import styles from "../styles";
+import { MdArrowOutward } from "react-icons/md";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y,Autoplay, FreeMode } from "swiper/modules";
 
+
+import "swiper/css";
+// import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
 const MarketPlace = () => {
   return (
     <section className={`py-20 sm:py-[10rem] ${styles.boxWidth}`}>
-      <div>
-        <h1
-          className={`text-white    sm:text-5xl uppercase text-3xl font-bold`}
-        >
-          create nfts <br />
-          marketplace <br />{" "}
-          <span className="font-light">for your community</span>
-        </h1>
-        <p className="text-white text-opacity-60 mt-[45px]">
-          Powered by <span className="font-bold uppercase">starx protocol</span>
-              </p>
-              <button className="bg-white py-[1rem] px-[1rem] rounded-full capitalize text-black font-semibold mt-[45px]">create marketplace <span></span></button>
-      </div>
+      <div className="grid md:grid-cols-2 lg:gap-x-[10rem] gap-y-16 md:gap-y-0 items-center">
+        <div className="lg:w-[150rem]">
+          <h1
+            className={`text-white    sm:text-5xl uppercase text-3xl font-bold leading-[3rem]`}
+          >
+            create nfts <br />
+            marketplace <br />{" "}
+            <span className="font-light">for your community</span>
+          </h1>
+          <p className="text-white text-opacity-60 mt-[45px]">
+            Powered by{" "}
+            <span className="font-bold uppercase">starx protocol</span>
+          </p>
+          <button className="bg-white py-[1rem] px-[1rem] rounded-full capitalize text-black flex items-center gap-x-2 font-semibold mt-[45px]">
+            create marketplace{" "}
+            <span className="font-bold text-xl">
+              <MdArrowOutward />
+            </span>
+          </button>
+        </div>
+        {/* <div className="grid gap-y-4"> */}
+          <Swiper
+            spaceBetween={50}
+                  slidesPerView={1}
+                  autoplay={{
+                      delay: 3000,
+                      disableOnInteraction:true
+                  }}
+                  speed={1000}
+            // navigation
+                  pagination={{ clickable: true }}
+                  modules={[FreeMode,Pagination,Autoplay,Navigation]}
+            // scrollbar={{ draggable: true }}
+                  className="w-[100%] pr-[80px] md:pr-[150px]"
+          >
+            {/* <SwiperSlide> */}
+              {marketPlace.map((item) => {
+                const { id, image, text, volume, flow, title } = item;
+                  return (
+                    <SwiperSlide>
+                      <article
+                        // key={id}
+                        className="bg-[#222323] p-[1rem] rounded-xl overflow-hidden max-w-[30rem] "
+                      >
+                        <img src={image} alt="" />
+                        <h3 className="text-white capitalize  text-2xl font-bold mt-4">
+                          {title}
+                        </h3>
+                        <p className="text-[#9E9E9E] mt-2">{text}</p>
+                        <div className="flex gap-x-6">
+                          <div className="text-white  grid ">
+                            <span className="capitalize mt-4 ">volume</span>
+                            {volume}
+                          </div>
+                          <div className="text-white  grid ">
+                            <span className="capitalize mt-4 ">flow</span>
+                            {flow}
+                          </div>
+                        </div>
+                      </article>
+                    </SwiperSlide>
+                  );
+              })}
+            {/* </SwiperSlide> */}
+          </Swiper>
+        </div>
+      {/* </div> */}
     </section>
   );
 }
